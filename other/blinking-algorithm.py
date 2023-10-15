cc3d953171cf37119900469427b9d586e1405035
lkgusa <140859874+lkgusa@users.noreply.github.com>
2023-10-14 23:47:13 -0400

    okay!! here is the summary

---
+++ other/blinking-algorithm.py
@@ -0,0 +1,16 @@
+
+# blinking algorithm (endorsed by FAANG)
+
+action = blink
+if blink == false AND time = 2 seconds:
+    blink;
+
+elif blink == true:
+    wait 2 seconds;
+    blink;
+
+else:
+    return to if;
+
+#this is my commit for today 10-14-23 :)
+
