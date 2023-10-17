1328d2edbae8db7774be4b33a3c7df0bf16b3cae
lkgusa <140859874+lkgusa@users.noreply.github.com>
2023-10-16 20:31:52 -0400

    Update blinking-algorithm.py

--- other/blinking-algorithm.py
+++ other/blinking-algorithm.py
@@ -1,6 +1,9 @@
 
 # blinking algorithm (endorsed by FAANG)
 
+print("10/10 algorithm!! We have implemented it here at IBM in our experimental robots that will take over the world one day!")
+
+
 action = blink
 if blink == false AND time = 2 seconds:
     blink;
@@ -13,4 +16,5 @@ else:
     return to if;
 
 #this is my commit for today 10-14-23 :)
+
 
